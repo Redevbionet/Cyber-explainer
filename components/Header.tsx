51b0122d79bdc519f3a7ce45d15927e7a9fb1692
CatRob satoshi Git <137897299+Catro-b@users.noreply.github.com>
2025-11-25 15:11:02 -0800

    Update Header.tsx
    Signed-off-by: CatRob satoshi Git <137897299+Catro-b@users.noreply.github.com>

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,8 +10,7 @@ const Header: React.FC = () => {
     { href: '#threats', text: 'รูปแบบภัยคุกคาม' },
     { href: '#advisor', text: 'ที่ปรึกษาด้านการป้องกัน' },
     { href: '#threat-briefing', text: 'สรุปภัยคุกคาม' },
-    { href: '#threat-briefing', text: 'สรุปภัยคุกคาม' },
-
+    { href: '#vulnerability-disclosure', text: 'นโยบายเปิดเผยช่องโหว่' },
   ];
 
   const handleLinkClick = () => {
